54b1984fa4db69b81cabfc4f97eed5b999928c28
ummaabdul23 <zahrahaani21@gmail.com>
2024-05-19 22:18:27 +0000

    python3

--- tests/test_file_storage.py
+++ tests/test_file_storage.py
@@ -2,21 +2,23 @@
 import models
 import unittest
 from models.base_model import BaseModel
-from models import storage
+from models.engine.file_storage import FileStorage
 import os
 
 
 class TestFileStorage(unittest.TestCase):
 
     def setUp(self):
+
         """
         Set up test environment
         """
         self.model = BaseModel()
-        storage.save()
+        self.storage.new(self.model)
         self.file_path = storage._FileStorage__file_path
 
     def tearDown(self):
+
         """
         Clean up test environment
         """
@@ -28,17 +30,30 @@ class TestFileStorage(unittest.TestCase):
         """
         Test save method of FileStorage
         """
-        storage.save()
+        self.storage.save()
         self.assertTrue(os.path.exists(self.file_path))
 
     def test_reload(self):
         """
         Test reload method of FileStorage
         """
-        storage.save()
-        storage._FileStorage__objects.clear()
-        storage.reload()
-        self.assertIn(f"BaseModel.{self.model.id}", storage.all())
+        self.storage.save()
+        self.storage._FileStorage__objects.clear()
+        self.storage.reload()
+        self.assertIn(f"BaseModel.{self.model.id}", self.storage.all())
+
+    def test_all(self):
+            """
+            Test the all method returns the correct objects.
+            """
+            self.assertEqual(dict, type(self.storage.all()))
+
+
+    def test_new(self):
+           """
+           Test the new method sets the correct object.
+           """
+           self.assertIn(f"BaseModel.{self.model.id}", self.storage.all())
 
 
 if __name__ == '__main__':
